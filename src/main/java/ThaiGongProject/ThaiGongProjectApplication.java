adedad0b9339674eaf08ee3482448391e5aa18d5
Hung Tran <tuanhung21051997@gmail.com>
2024-02-24 16:49:43 +1100

    Initial commit

---
+++ src/main/java/ThaiGongProject/ThaiGongProjectApplication.java
@@ -0,0 +1,13 @@
+package ThaiGongProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaiGongProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaiGongProjectApplication.class, args);
+	}
+
+}
